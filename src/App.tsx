4b820729a013b14cca6544ddae386f4d768018b4
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-05-09 10:32:06 +0530

    Initialise: Initialise authentication by adding form validation

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import RouteProtection from "./Components/RouteProtection/RouteProtection";
+import ViewAllProducts from "./Components/ViewAllProducts/ViewAllProducts";
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/SignUp/SignUp";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<RouteProtection />}>
+            <Route path="/" element={<ViewAllProducts />}></Route>
+          </Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/SignUp/SignUp.tsx
@@ -0,0 +1,170 @@
+import {
+  Typography,
+  TextField,
+  Button,
+  InputAdornment,
+  Stack,
+  IconButton,
+} from "@mui/material";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { useState } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { red } from "@mui/material/colors";
+import { useNavigate } from "react-router-dom";
+
+const schema = z.object({
+  name: z
+    .string()
+    .refine(
+      (value) => /^[a-zA-Z]+\s+[a-zA-Z]+$/.test(value ?? ""),
+      "Please enter both firstname and lastname"
+    ),
+  email: z.string().email(),
+  phoneNumber: z
+    .string()
+    .refine(
+      (value) => /^(\d{10})$/.test(value ?? ""),
+      "Please Enter a 10 digit number"
+    ),
+  password: z
+    .string()
+    .refine(
+      (value) =>
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(
+          value ?? ""
+        ),
+      `Enter Proper password having minimum 8 length, atleast 1 uppercase, 1 lowercase, 1 special character`
+    ),
+});
+
+type UserFormField = z.infer<typeof schema>;
+
+export default function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const handleClickOnShowPassword = () => {
+    setShowPassword((show) => !show);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<UserFormField>({
+    resolver: zodResolver(schema),
+  });
+
+  const slotPropsForPassword = {
+    input: {
+      endAdornment: (
+        <InputAdornment position="end">
+          <IconButton
+            aria-label={
+              showPassword ? "hide the password" : "display the password"
+            }
+            onClick={handleClickOnShowPassword}
+            edge="end"
+          >
+            {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+          </IconButton>
+        </InputAdornment>
+      ),
+    },
+  };
+
+  const slotPropsForPhoneNumber = {
+    input: {
+      startAdornment: <InputAdornment position="start">+91</InputAdornment>,
+    },
+  };
+
+  const onSubmit: SubmitHandler<UserFormField> = (data) => {
+    let users = JSON.parse(localStorage.getItem("users-array") as string);
+    users.push({ ...data, id: crypto.randomUUID() });
+    navigate("/");
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack
+        direction={"column"}
+        spacing={3}
+        maxWidth={"600px"}
+        margin={"auto"}
+        paddingLeft={"40px"}
+        paddingRight={"40px"}
+        paddingTop={"100px"}
+      >
+        <Typography variant="h4" textAlign={"center"}>
+          Sign Up
+        </Typography>
+        <TextField
+          {...register("name")}
+          type="text"
+          label="Full Name"
+          variant="outlined"
+          color="primary"
+          size="medium"
+          required
+        ></TextField>
+        {errors.name && (
+          <Typography color={red[500]}>{errors.name.message}</Typography>
+        )}
+        <TextField
+          {...register("email")}
+          type="email"
+          label="Email"
+          variant="outlined"
+          color="primary"
+          size="medium"
+          required
+        ></TextField>
+        {errors.email && (
+          <Typography color={red[500]}>{errors.email.message}</Typography>
+        )}
+
+        <TextField
+          {...register("phoneNumber")}
+          type="text"
+          label="Phone Number"
+          variant="outlined"
+          color="primary"
+          slotProps={slotPropsForPhoneNumber}
+          size="medium"
+          required
+        ></TextField>
+        {errors.phoneNumber && (
+          <Typography color={red[500]}>{errors.phoneNumber.message}</Typography>
+        )}
+
+        <TextField
+          {...register("password")}
+          type={showPassword ? "text" : "password"}
+          label="Password"
+          slotProps={slotPropsForPassword}
+          variant="outlined"
+          color="primary"
+          size="medium"
+          helperText="Do not share your password with anyone"
+          required
+        ></TextField>
+        {errors.password && (
+          <Typography color={red[500]}>{errors.password.message}</Typography>
+        )}
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          size="large"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? "Loading..." : "Sign Up"}
+        </Button>
+      </Stack>
+    </form>
+  );
+}
