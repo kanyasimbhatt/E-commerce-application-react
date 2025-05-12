a05317c10d5d5243408f216fec65e4f559ec7813
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-05-12 16:06:33 +0530

    add: add code for share product information

--- src/Components/ViewAllProducts/ViewAllProducts.tsx
+++ src/Components/ViewAllProducts/ViewAllProducts.tsx
@@ -2,7 +2,10 @@ import { useEffect, useState } from "react";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import { Typography, Card, Button, Stack } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { Typography, Card, Button, Stack, IconButton } from "@mui/material";
+import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
 import Navbar from "../Navbar/Navbar";
 
 type Product = {
@@ -20,6 +23,9 @@ type InputObject = {
 
 export default function ViewAllProducts() {
   const [products, setProducts] = useState<Array<Product>>([]);
+  const [showShare, setShowShare] = useState(false);
+  const [saveClipboard, setSaveClipboard] = useState(false);
+  const [productIdSelected, setProductIdSelected] = useState(0);
   async function getProducts() {
     try {
       const response = await fetch("https://dummyjson.com/products");
@@ -32,6 +38,23 @@ export default function ViewAllProducts() {
       console.log(err);
     }
   }
+
+  const handleClickOnShare = (productId: number) => {
+    setProductIdSelected(productId);
+    setShowShare((share) => !share);
+  };
+
+  const handleClickOnClose = () => {
+    setShowShare((share) => !share);
+  };
+
+  const handleSaveToClipboard = async () => {
+    await window.navigator.clipboard.writeText(
+      `http://localhost:5175/product/${productIdSelected}`
+    );
+    setSaveClipboard((save) => !save);
+    setTimeout(() => setSaveClipboard((save) => !save), 3000);
+  };
 
   useEffect(() => {
     getProducts();
@@ -69,12 +92,57 @@ export default function ViewAllProducts() {
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small">Share</Button>
+              <Button
+                size="small"
+                onClick={() => handleClickOnShare(product.id)}
+              >
+                Share
+              </Button>
               <Button size="small">Learn More</Button>
             </CardActions>
           </Card>
         ))}
       </Stack>
+
+      {showShare && (
+        <Stack
+          position={"fixed"}
+          width={"100%"}
+          height={"100vh"}
+          top={0}
+          paddingLeft={"35%"}
+          paddingTop={"20%"}
+          zIndex={3}
+          sx={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+        >
+          <Stack
+            borderRadius={"7px"}
+            gap={3}
+            height={"150px"}
+            width={"400px"}
+            padding={"40px"}
+            sx={{ backgroundColor: "rgba(255, 255, 255, 0.9)" }}
+          >
+            <IconButton
+              onClick={handleClickOnClose}
+              sx={{ display: "flex", justifyContent: "end", width: "40px" }}
+            >
+              <CloseIcon />
+            </IconButton>
+
+            <Typography variant="h6" gap={3} textAlign={"center"}>
+              Copy Link to Clipboard{" "}
+              <IconButton onClick={handleSaveToClipboard}>
+                {saveClipboard ? (
+                  <CheckCircleRoundedIcon color="success" />
+                ) : (
+                  <ContentCopyIcon />
+                )}
+              </IconButton>
+            </Typography>
+          </Stack>
+        </Stack>
+      )}
     </>
   );
 }
